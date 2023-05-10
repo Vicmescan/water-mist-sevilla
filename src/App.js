7c0bfaaebb83dd8308f517ef441a935f547ea8c4
vicmescan <mestrecanto@gmail.com>
2023-05-10 12:55:12 +0200

    trying to solve react router problems when deploy

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React from "react";
-
-import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/common/Header";
 import AnimatedRoutes from "./components/common/AnimatedRoutes";
-
 import "./index.css";
 
-
 function App() {
-
   return (
     <div className="app">
-        <Header />
-        <AnimatedRoutes />
+      <Header />
+      <AnimatedRoutes />
     </div>
   );
 }
